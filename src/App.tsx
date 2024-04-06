fa9a9a53c19fb49b93271f4c1e9443208bc42938
atayikilmaz <ata.yikilmaz@gmail.com>
2024-04-06 22:21:09 +0300

    changed to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 // App.tsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UrlCreator from './Components/UrlCreator';
 import Redirector from './Components/Redirector'; 
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" Component={UrlCreator} />
-        <Route path="/:shortenedUrlKey" Component={Redirector} />
+        <Route path="/:shortenedUrlKey" Component={Redirector} /> {/* This route will match any shortened URL */}
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
